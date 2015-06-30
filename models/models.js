ab7b3afe4007f3e656467e9a293c600138fbbbd9
sjdop <juandop@hotmail.es>
2015-06-30 09:12:45 -0500

    correccion de conexion

--- models/models.js
+++ models/models.js
@@ -10,16 +10,23 @@ var protocol 	= (url[1]||null);
 var dialect 	= (url[1]||null);
 var port 		= (url[5]||null);
 var host 		= (url[4]||null);
-var storage 	= process.evn.DATABASE_STORAGE;
+var storage 	= process.env.DATABASE_STORAGE;
 
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite
-var sequelize = new Sequelize(null, null, null,
-						{ dialect: "sqlite", storage: "quiz.sqlite"}
-					);
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+	{
+		dialect: 	protocol,
+		protocol: 	protocol,
+		port: 		port,
+		host: 		host,
+		storage: 	storage,	// solo SQLite (.env)
+		omitNull: 	true		// solo Postgres
+	}
+);
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
