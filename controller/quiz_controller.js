8df51fe86e6945f356aa59d2b00bed0b8e600ef4
sjdop <juandop@hotmail.es>
2015-07-08 16:07:50 -0500

    Modulo 8, comienzo de implementacion de la actividad

--- controller/quiz_controller.js
+++ controller/quiz_controller.js
@@ -15,29 +15,89 @@ exports.load = function(req, res, next, quizId) {
 exports.index = function(req, res, next) {
 	if(req.query.search != null) {
 		//Funcion de busqueda
-		var search = "%" + req.query.search.replace(" ", "%") + "%";
+		var search = "%" + req.query.search.replace(/\s/g, "%") + "%";
 		models.Quiz.findAll({where:["pregunta like ?", search]}).then(function(quizes) {
-			res.render('quizes/index.ejs', { quizes: quizes});
+			res.render('quizes/index.ejs', { quizes: quizes, errors: []});
 		}).catch(function(error) { next(error); });
 	} else {
 		models.Quiz.findAll().then(function(quizes) {
-			res.render('quizes/index.ejs', { quizes: quizes});
+			res.render('quizes/index.ejs', { quizes: quizes, errors: []});
 		}).catch(function(error) { next(error); });
 	}
 };
 
 exports.show = function(req, res) {
-	res.render('quizes/show', { quiz: req.quiz});
+	res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 exports.answer = function(req, res) {
 	var resultado = "Incorrecto";
-	if ( req.query.respuesta == req.quiz.respuesta) {
+	if ( req.query.respuesta === req.quiz.respuesta) {
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', 
+		{ quiz: req.quiz,
+			respuesta: resultado,
+			errors: []
+		});
 };
 
 exports.author = function(req, res) {
-	res.render('author/author', {nombre: 'Juan David', apellidos: 'Osorio Posada'});
+	res.render('author/author', {nombre: 'Juan David', apellidos: 'Osorio Posada', errors: []});
+}
+
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( //crea el objeto quiz
+		{ pregunta: "Pregunta", respuesta: "Respuesta" }
+	);
+	res.render('quizes/new', {quiz: quiz, errors: []});
+}
+
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	
+	var errors = quiz.validate();
+	if(errors) {
+		var i = 0;
+		var errores = new Array();
+		for(var prop in errors) {
+			errores[i++] = { message: errors[prop]};
+		}
+		res.render('quizes/new', {quiz: quiz, errors: errores});
+	} else {
+		quiz.save({fields: ["pregunta", "respuesta"]})
+		.then(
+			function() {
+				res.redirect('/quizes');
+			})
+	}
+};
+
+exports.edit = function(req, res) {
+	var quiz = req.quiz; //autoload de la instancia de quiz
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+}
+
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	var errors = req.quiz.validate();
+	if(errors) {
+		var i = 0;
+		var errores = new Array();
+		for(var prop in errors) {
+			errores[i++] = { message: errors[prop]};
+		}
+		res.render('quizes/edit', { quiz: req.quiz, errors: errores});
+	} else {
+		req.quiz.save( {fields: ["pregunta", "respuesta"]})
+		.then( function() { res.redirect('/quizes')});
+	}
+}
+
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then( function() {
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
 }
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controller/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 router.param('quizId', quizController.load); // autoload :quizId
@@ -15,6 +15,16 @@ router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+router.get('/quizes/new', quizController.new);
+
+router.post('/quizes/create', quizController.create);
+
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/author', quizController.author);
 
